d070c9c548b79303cbd2f6076cdf24d9c9211f6b
Bill B <billborkowski7@gmail.com>
2021-12-13 01:36:27 -0500

    Added all of Chris's stuff, will now merge into Development

---
+++ src/Components/Admin/adminSidebar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './NavigationStyles.css';
+import {SidebarData} from './adminSidebarData';
+
+function adminSidebar() {
+    return (
+        <div className="Sidebar">
+            <ul className='SidebarList'>
+              {SidebarData.map((val, key)=> {
+                  return(
+                    <li 
+                        key={key} 
+                        className='row'
+                        id={window.location.pathname === val.link ? "active" : ""}
+                        onClick ={() => {window.location.pathname = val.link;
+                        }}
+                    >
+                        <div id="icon">{val.icon}</div>{" "}
+                        <div id="title">
+                            {val.title}
+                        </div>
+                    </li>
+                  );
+                })}
+            </ul>
+        </div>
+    );
+}
+
+export default adminSidebar
