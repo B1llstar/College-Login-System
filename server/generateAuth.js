21c278fcbf47399465a2b13a28a9a53ea9150e21
Chris S <cms3110@gmail.com>
2021-12-18 02:28:30 -0500

    Auto stash before merge of "Chris_Dev" and "origin/B1ll_Dev"

--- server/generateAuth.js
+++
@@ -1,9 +0,0 @@
-const UIDGenerator = require("uid-generator");
-const uidgen = new UIDGenerator();
-
-function generateAuthToken() {
-  const auth = uidgen.generateSync();
-  return auth;
-}
-
-module.exports.generateAuthToken = generateAuthToken;
